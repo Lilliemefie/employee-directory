decd6663241a753bb394fb2479df4b6a5bb61004
lillie <lillie@Lillies-MBP.home>
2021-08-02 23:54:43 -0700

    updated files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Main from "./components/Main";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
